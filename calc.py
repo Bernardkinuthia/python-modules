e308c12bbb14b1f54c7edb130d9b057cf986818e
Bernardkinuthia <kinuthia9@gmail.com>
2024-12-13 11:32:53 -0800

    third payrty  module

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
 def add(x,y):
     print(x+y)
 
-    def substract(x,y):
+def substract(x,y):
     print(x-y)
--- modulesOne.py
+++ modulesOne.py
@@ -1,4 +1,14 @@
-def greet():
-    print('hi')
+# import calc
+# from math import sqrt,factorial
 
-# hello
+
+# calc.add(56,24)
+# calc.add(46,24)
+# calc.substract(50,20)
+
+# print(sqrt(16))
+
+import requests
+response = requests.get("https://api.github.com")
+print(response.status_code) # output: 200
+print(response.json()) # Output: API response in JSON format
